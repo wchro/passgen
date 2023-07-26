1401755b66ce41bb49980089de4865ad220f52c9
wchro <93561159+wchro@users.noreply.github.com>
2023-07-26 22:09:28 +0200

    final version

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,6 +2,7 @@ const password_box = document.getElementById("password");
 const gen_btn = document.querySelector(".generate-btn");
 
 const pass_len = document.querySelector(".password-len");
+const passwdScore = document.querySelector(".password-score");
 
 const letters = Array.from({ length: 26 }, (_, i) =>
   String.fromCharCode(97 + i)
@@ -46,9 +47,52 @@ const symbols = [
   "`",
   "~",
   "_",
+  "\\",
 ];
 
 const randomChoice = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const passwordStrengthMeter = (password) => {
+  let passwordScore = 0;
+
+  if (password.length >= 16) {
+    passwordScore += 25;
+  } else if (password.length >= 12) {
+    passwordScore += 10;
+  } else {
+    passwordScore += 5;
+  }
+
+  /[a-z]/.test(password) && /[A-Z]/.test(password) && (passwordScore += 25);
+
+  /[0-9]/.test(password) && (passwordScore += 25);
+
+  /[!@#$%&*()\-+=\[\]{}|;:,\.\/?<>"'^`~_\\]/.test(password) &&
+    (passwordScore += 25);
+
+  let chkpass = "";
+
+  for (let i = 0; i < password.length; i++) {
+    if (chkpass.includes(password[i])) {
+      passwordScore -= 10;
+    }
+    chkpass += password[i];
+    console.log(chkpass);
+  }
+
+  if (passwordScore >= 75) {
+    passwdScore.style.backgroundColor = "chartreuse";
+  } else if (passwordScore >= 50) {
+    passwdScore.style.backgroundColor = "darkgreen";
+  } else if (passwordScore >= 25) {
+    passwdScore.style.backgroundColor = "darkorange";
+  } else {
+    passwdScore.style.backgroundColor = "darkred";
+  }
+
+  if (passwordScore <= 0) passwordScore = 5;
+  passwdScore.style.width = `${passwordScore}%`;
+};
 
 const generatePassword = () => {
   let configuration = {
@@ -85,14 +129,15 @@ const generatePassword = () => {
       password += chars[current_index];
       chars.splice(current_index, 1);
     }
-
+    passwordStrengthMeter(password);
     return password;
   }
-
+  passwdScore.style.width = `0%`;
   return "🤔";
 };
 
 password_box.value = generatePassword();
+pass_len.textContent = document.getElementById("password-length").value;
 
 gen_btn.addEventListener(
   "click",
